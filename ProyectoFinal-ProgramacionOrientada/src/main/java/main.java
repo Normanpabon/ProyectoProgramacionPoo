4cb67c0a0212ba60ed6badbe10fc5204a85096c4
Norman_P <45250874+Normanpabon@users.noreply.github.com>
2021-05-21 10:06:10 -0500

    Refactoring

---
+++ ProyectoFinal-ProgramacionOrientada/src/main/java/main.java
@@ -0,0 +1,12 @@
+import Pacientes.Paciente;
+import java.util.ArrayList;
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
